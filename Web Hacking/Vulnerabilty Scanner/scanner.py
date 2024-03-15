90f2009028e5a3cb97c5aac68b1ea61fa0563279
Vishesh Singh <134791580+visheshoffice@users.noreply.github.com>
2024-03-15 16:22:06 +0530

    Update scanner.py

--- Web Hacking/Vulnerabilty Scanner/scanner.py
+++ Web Hacking/Vulnerabilty Scanner/scanner.py
@@ -1,7 +1,7 @@
 import requests
 import re
-import urlparse
-from BeautifulSoup import BeautifulSoup
+import urllib.parse as urlparse
+from bs4 import BeautifulSoup
 
 class Scanner:
 	def __init__(self, url, ignore_links):
@@ -9,12 +9,12 @@ class Scanner:
 		self.target_url = url
 		self.target_links = []
 		self.links_to_ignore = ignore_links
-	
-	def extract_links_from(self, url):	
-		response = self.session.get(target_url)
-		return re.findall('(?:href=")(.*?)"', response.content)
 
-	def crawl(self, url = None):
+	def extract_links_from(self, url):
+		response = self.session.get(url)
+		return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore"))
+
+	def crawl(self, url=None):
 		if url == None:
 			url = self.target_url
 		href_links = self.extract_links_from(url)
@@ -26,7 +26,7 @@ class Scanner:
 
 			if self.target_url in link and link not in self.target_links and link not in self.links_to_ignore:
 				self.target_links.append(link)
-				print(link)
+				print(link + "\n")
 				self.crawl(link)
 
 	def extract_forms(self, url):
@@ -55,27 +55,29 @@ class Scanner:
 
 	def run_scanner(self):
 		for link in self.target_links:
-			form = self.extract_forms(link)
+			forms = self.extract_forms(link)
 			for form in forms:
-				print("Testing the form in "+ link)
+				print("\n\n[+] Testing form in --> " + link)
 				is_vulnerable_to_xss = self.test_xss_in_form(form, link)
 				if is_vulnerable_to_xss:
-					print("\n\n XSS discovered in "+ link + "in the following form")
+					print("\n\n[***] XSS Vulnerability Discovered In --> " + link + " In The Following Form: \n\n")
 					print(form)
 
 			if "=" in link:
-				print("Testing "+ link)
+				print("\n\n[+] Testing --> " + link)
 				is_vulnerable_to_xss = self.test_xss_in_link(link)
 				if is_vulnerable_to_xss:
-					print("\n\n XSS discovered in "+ link)
+					print("\n\n[***] XSS Vulnerability Discovered In --> " + link + "\n\n")
 
 	def test_xss_in_link(self, url):
 		xss_test_script = "<sCript>alert('test')</scriPt>"
 		url = url.replace("=", "=" + xss_test_script)
 		response = self.session.get(url)
-		return xss_test_script in response.content
+
+		return xss_test_script in response.content.decode()
 
 	def test_xss_in_form(self, form, url):
 		xss_test_script = "<sCript>alert('test')</scriPt>"
-		response = self.submit.form(form, xss_test_script, url)
-		return xss_test_script in response.content
+		response = self.submit_form(form, xss_test_script, url)
+
+		return xss_test_script in response.content.decode()
